92af50e79040fbb13c7eea0adc97a444765d0714
Maximiliano Vargas <Shilua4@gmail.com>
2021-05-07 20:56:50 -0300

    first commit

---
+++ pps-chat/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDlu-sAoN3GAJL4K5HMaEzEX_G6HUBvyLA",
+    authDomain: "apps-pps-61952.firebaseapp.com",
+    databaseURL: "https://apps-pps-61952.firebaseio.com",
+    projectId: "apps-pps-61952",
+    storageBucket: "apps-pps-61952.appspot.com",
+    messagingSenderId: "977550240551",
+    appId: "1:977550240551:web:41c701cc4f1240a6fe9702",
+    measurementId: "G-9FC997FPKN"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
